52d737d58ee04519ad8a4c4175eeb25928d42b61
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-01 22:22:27 +0530

    feat: update test module info

--- src/test/java/ds/module-info.java
+++ src/test/java/ds/module-info.java
@@ -1,12 +1,16 @@
 open module ds {
   requires java.logging;
-  requires org.mockito;
-  requires net.bytebuddy.agent;
+  requires java.compiler;
+  requires java.management;
+  requires net.bytebuddy; // required for mockito to work with JPMS.
+  requires net.bytebuddy.agent; // required for mockito to work with JPMS.
   requires transitive org.junit.jupiter.api;
+  requires transitive org.junit.jupiter.engine;
   requires transitive org.junit.jupiter.params;
-  requires transitive org.junit.platform.launcher;
   requires transitive org.junit.platform.commons;
   requires transitive org.junit.platform.engine;
+  requires transitive org.junit.platform.launcher;
+  requires org.mockito;
 
   exports ds;
 }
